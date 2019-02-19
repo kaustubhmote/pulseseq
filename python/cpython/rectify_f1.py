83d4cd046d6a2fe4970cf0b263cc602b81d846ee
Kaustubh R Mote <kaustubh.mote@gmail.com>
2019-02-19 18:55:55 +0530

    rectify_f1 now picks the correct nucleus from a dropdown list of nuclei available

--- python/cpython/rectify_f1.py
+++ python/cpython/rectify_f1.py
@@ -21,33 +21,42 @@ name, curdir, curexpno, curprocno = argv
 
 # Get output directory from user
 oexpno = curexpno + '00' 
-iexpno, oexpno, f1_nucleus, f1_sfo, overwrite = dialog(
+iexpno, oexpno, f1_nuc, overwrite = dialog(
                   header='Rectify F1 dimension parameters',
                   info='Correct the parameters for the F1 dimension',
-                  labels=['Processed dataset number to correct',
-                          'PROCNO for the output data',
-                          'New F1 Nucleus',
-                          'New F1 Spectrometer Frequency (SFO)',
+                  labels=['EXPNO of the dataset to correct',
+                          'EXPNO of the output dataset',
+                          'Correct F1 nucleus (Channel number from \'EDASP\')',
                           'Overwrite'],
-                  types=['e', 'e', 'e', 'e', 'c'],
-                  values=[curexpno, oexpno, '', '', ''],
-                  comments=['', '', '', '', ''])
+                  types=['e', 'e', 'd', 'c'],
+                  values=[curexpno, oexpno, list(range(1, 7)), ''],
+                  comments=['', '', '', ''])
+
+
+# see if overwriting is allowed
+if 'selected' in overwrite:
+    overwrite = True
+else:
+    overwrite = False
 
 
 indir = os.path.join(curdir, iexpno)
 outdir = os.path.join(curdir, oexpno)
+
 
 # read the data
 dic, data = ng.bruker.read(indir, read_acqus=True)
 
 
 # change the processing frequency for F1 dimension
-dic['acqu2s']['NUC1'] = dic['acqus']['NUC3']
-dic['acqu2s']['BF1'] = dic['acqus']['BF3']
-dic['acqu2s']['O1'] = dic['acqus']['O3']
-dic['acqu2s']['SFO1'] = dic['acqus']['SFO3']
-dic['acqu2s']['SW'] *= dic['acqus']['BF1'] / dic['acqus']['BF3']
-dic['proc2s']['SF'] = dic['acqus']['SFO3']
+dic['acqu2s']['NUC1'] = dic['acqus'][f'NUC{f1_nuc}']
+dic['acqu2s']['SW'] *= dic['acqu2s']['BF1'] / dic['acqus'][f'BF{f1_nuc}']
+dic['acqu2s']['BF1'] = dic['acqus'][f'BF{f1_nuc}']
+dic['acqu2s']['O1'] = dic['acqus'][f'O{f1_nuc}']
+dic['acqu2s']['SFO1'] = dic['acqus'][f'SFO{f1_nuc}']
+dic['proc2s']['SF'] = dic['acqus'][f'SFO{f1_nuc}']
 
+
+# write out the file
 ng.bruker.write(outdir, dic, data, write_prog=False, write_acqus=True, 
-                write_procs=True, pdata_folder=True)
+                write_procs=True, pdata_folder=True, overwrite=overwrite)
