b9a413c8dcc747e3ab0225b47b357cfef04f52ad
Kaustubh R. Mote <kaustubh.mote@gmail.com>
2020-08-04 14:07:27 +0530

    black formatted, removed debugging print and additional info for Nucleus to correct

--- python/cpython/rectify_fn.py
+++ python/cpython/rectify_fn.py
@@ -1,4 +1,4 @@
-'''
+"""
 rectify_f1.py: correct the f1 dimension spectral width and frequency
 
 
@@ -9,7 +9,7 @@ rectify_f1.py: correct the f1 dimension spectral width and frequency
  Bugs and suggestions
  --------------------
  kaustuberm @ tifrh.res.in
-'''
+"""
 
 import os
 import nmrglue as ng
@@ -23,18 +23,21 @@ Flist = [f"F{i}" for i in range(1, 7)]
 
 
 # Get output directory from user
-oexpno = curexpno + '00' 
+oexpno = curexpno + "00"
 iexpno, oexpno, nuc, corr_nuc, overwrite = dialog(
-                  header='Rectify F1/2 dimension parameters',
-                  info='Correct the parameters for the F1 or F2 dimension',
-                  labels=['EXPNO of the dataset to correct',
-                          'EXPNO of the output dataset',
-                          'Nucleus to correct',
-                          'Correct nucleus (Channel number from \'EDASP\')',
-                          'Overwrite'],
-                  types=['e', 'e', 'd', 'd', 'c'],
-                  values=[curexpno, oexpno, Flist, Flist, ''],
-                  comments=['', '', '', '', ''])
+    header="Rectify F1/2 dimension parameters",
+    info="Correct the parameters for the F1 or F2 dimension",
+    labels=[
+        "EXPNO of the dataset to correct",
+        "EXPNO of the output dataset",
+        "Nucleus to correct (Dimension from ACQUPARS tab)",
+        "Correct nucleus (Channel number from 'EDASP')",
+        "Overwrite",
+    ],
+    types=["e", "e", "d", "d", "c"],
+    values=[curexpno, oexpno, Flist, Flist, ""],
+    comments=["", "", "", "", ""],
+)
 
 
 # Sanitize
@@ -45,7 +48,7 @@ corr_nuc = int(corr_nuc[1])
 
 
 # see if overwriting is allowed
-if 'selected' in overwrite or overwrite:
+if "selected" in overwrite or overwrite:
     overwrite = True
 else:
     overwrite = False
@@ -58,16 +61,24 @@ outdir = os.path.join(curdir, oexpno)
 # read the data
 dic, data = ng.bruker.read(indir, read_acqus=True, read_pulseprogram=False)
 
-print(acquns_file, procns_file, corr_nuc)
+
 # change the processing frequency for appropriate dimension
-dic[acquns_file]['NUC1'] = dic['acqus'][f'NUC{corr_nuc}']
-dic[acquns_file]['SW'] *= dic[acquns_file]['BF1'] / dic['acqus'][f'BF{corr_nuc}']
-dic[acquns_file]['BF1'] = dic['acqus'][f'BF{corr_nuc}']
-dic[acquns_file]['O1'] = dic['acqus'][f'O{corr_nuc}']
-dic[acquns_file]['SFO1'] = dic['acqus'][f'SFO{corr_nuc}']
-dic[procns_file]['SF'] = dic['acqus'][f'SFO{corr_nuc}']
+dic[acquns_file]["NUC1"] = dic["acqus"][f"NUC{corr_nuc}"]
+dic[acquns_file]["SW"] *= dic[acquns_file]["BF1"] / dic["acqus"][f"BF{corr_nuc}"]
+dic[acquns_file]["BF1"] = dic["acqus"][f"BF{corr_nuc}"]
+dic[acquns_file]["O1"] = dic["acqus"][f"O{corr_nuc}"]
+dic[acquns_file]["SFO1"] = dic["acqus"][f"SFO{corr_nuc}"]
+dic[procns_file]["SF"] = dic["acqus"][f"SFO{corr_nuc}"]
 
 
 # write out the file
-ng.bruker.write(outdir, dic, data, write_prog=False, write_acqus=True, 
-                write_procs=True, pdata_folder=True, overwrite=overwrite)
+ng.bruker.write(
+    outdir,
+    dic,
+    data,
+    write_prog=False,
+    write_acqus=True,
+    write_procs=True,
+    pdata_folder=True,
+    overwrite=overwrite,
+)
