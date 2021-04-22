9ae1af9e2f71652aaf558a056340588548233a7e
Kaustubh R. Mote <kaustubh.mote@gmail.com>
2021-04-22 12:36:14 +0530

    multiple nuclei can now be corrected at the same time

--- python/cpython/rectify_fn.py
+++ python/cpython/rectify_fn.py
@@ -19,32 +19,47 @@ from base import dialog
 
 name, curdir, curexpno, curprocno = argv
 
-Flist = [f"F{i}" for i in range(1, 7)]
+Flist = ["None"] + [f"F{i}" for i in range(1, 7)]
 
 
 # Get output directory from user
 oexpno = curexpno + "00"
-iexpno, oexpno, nuc, corr_nuc, overwrite = dialog(
+iexpno, oexpno, nuc1, corr_nuc1, nuc2, corr_nuc2, overwrite = dialog(
     header="Rectify F1/2 dimension parameters",
     info="Correct the parameters for the F1 or F2 dimension",
     labels=[
         "EXPNO of the dataset to correct",
         "EXPNO of the output dataset",
-        "Nucleus to correct (Dimension from ACQUPARS tab)",
-        "Correct nucleus (Channel number from 'EDASP')",
+        "Nucleus to correct (Dimension from ACQUPARS tab) I",
+        "Correct nucleus (Channel number from 'EDASP') I",
+        "Nucleus to correct (Dimension from ACQUPARS tab) II",
+        "Correct nucleus (Channel number from 'EDASP') II",
         "Overwrite",
     ],
-    types=["e", "e", "d", "d", "c"],
-    values=[curexpno, oexpno, Flist, Flist, ""],
-    comments=["", "", "", "", ""],
+    types=["e", "e", "d", "d", "d", "d", "c"],
+    values=[curexpno, oexpno, Flist, Flist, Flist, Flist, ""],
+    comments=["", "", "", "", "", "", ""],
 )
 
+corr_nuc_list = []
+afiles = []
+pfiles = []
 
 # Sanitize
-nuc = int(nuc[1])
-acquns_file = f"acqu{nuc+1}s"
-procns_file = f"proc{nuc+1}s"
-corr_nuc = int(corr_nuc[1])
+if nuc1 != "None":
+    nuc1 = int(nuc1[1])
+    corr_nuc1 = int(corr_nuc1[1])
+    corr_nuc_list.append(corr_nuc1)
+    afiles.append(f"acqu{nuc1+1}s")
+    pfiles.append(f"proc{nuc1+1}s")
+
+
+if nuc2 != "None":
+    nuc2 = int(nuc2[1])
+    corr_nuc2 = int(corr_nuc2[1])
+    corr_nuc_list.append(corr_nuc2)
+    afiles.append(f"acqu{nuc2+1}s")
+    pfiles.append(f"proc{nuc2+1}s")
 
 
 # see if overwriting is allowed
@@ -54,31 +69,36 @@ else:
     overwrite = False
 
 
-indir = os.path.join(curdir, iexpno)
-outdir = os.path.join(curdir, oexpno)
+if len(corr_nuc_list) == 0:
+    print("Nothing to do. Exiting ...")
 
+else:
 
-# read the data
-dic, data = ng.bruker.read(indir, read_acqus=True, read_pulseprogram=False)
+    indir = os.path.join(curdir, iexpno)
+    outdir = os.path.join(curdir, oexpno)
 
+    # read the data
+    dic, data = ng.bruker.read(indir, read_acqus=True, read_pulseprogram=False)
 
-# change the processing frequency for appropriate dimension
-dic[acquns_file]["NUC1"] = dic["acqus"][f"NUC{corr_nuc}"]
-dic[acquns_file]["SW"] *= dic[acquns_file]["BF1"] / dic["acqus"][f"BF{corr_nuc}"]
-dic[acquns_file]["BF1"] = dic["acqus"][f"BF{corr_nuc}"]
-dic[acquns_file]["O1"] = dic["acqus"][f"O{corr_nuc}"]
-dic[acquns_file]["SFO1"] = dic["acqus"][f"SFO{corr_nuc}"]
-dic[procns_file]["SF"] = dic["acqus"][f"SFO{corr_nuc}"]
+    for corr_nuc, acquns_file, procns_file in zip(corr_nuc_list, afiles, pfiles):
+        # change the processing frequency for appropriate dimension
+        dic[acquns_file]["NUC1"] = dic["acqus"][f"NUC{corr_nuc}"]
+        dic[acquns_file]["SW"] *= (
+            dic[acquns_file]["BF1"] / dic["acqus"][f"BF{corr_nuc}"]
+        )
+        dic[acquns_file]["BF1"] = dic["acqus"][f"BF{corr_nuc}"]
+        dic[acquns_file]["O1"] = dic["acqus"][f"O{corr_nuc}"]
+        dic[acquns_file]["SFO1"] = dic["acqus"][f"SFO{corr_nuc}"]
+        dic[procns_file]["SF"] = dic["acqus"][f"SFO{corr_nuc}"]
 
-
-# write out the file
-ng.bruker.write(
-    outdir,
-    dic,
-    data,
-    write_prog=False,
-    write_acqus=True,
-    write_procs=True,
-    pdata_folder=True,
-    overwrite=overwrite,
-)
+    # write out the file
+    ng.bruker.write(
+        outdir,
+        dic,
+        data,
+        write_prog=False,
+        write_acqus=True,
+        write_procs=True,
+        pdata_folder=True,
+        overwrite=overwrite,
+    )
